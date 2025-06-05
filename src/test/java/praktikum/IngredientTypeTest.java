2f6b3106a476b3965fefde04cca9eedcb51e427d
Andrey Dementev <andreasdv1@yandex.ru>
2025-06-05 14:52:27 +0300

    Commit6

---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,21 @@
+package praktikum;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IngredientTypeTest {
+
+    @Test
+    public void testValues() {
+        IngredientType[] values = IngredientType.values();
+        assertEquals(2, values.length);
+        assertEquals(IngredientType.SAUCE, values[0]);
+        assertEquals(IngredientType.FILLING, values[1]);
+    }
+
+    @Test
+    public void testValueOf() {
+        assertEquals(IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
+        assertEquals(IngredientType.FILLING, IngredientType.valueOf("FILLING"));
+    }
+}
